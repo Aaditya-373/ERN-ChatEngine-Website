5fc470a0934afee01a136d4850a208ab7da8a1a1
Aaditya-373 <100772439+Aaditya-373@users.noreply.github.com>
2024-06-28 00:57:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:["https://react-chat-engine.vercel.app/"],
+        origin:["http://localhost:5173"],
         methods:["POST","GET","PUT","DELETE"],
         credentials:true
     }
